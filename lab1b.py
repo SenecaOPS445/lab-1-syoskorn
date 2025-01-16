996920e177470698f1d037a544f67c09af32b97d
Sanisorn Yoskorn <syoskorn@myseneca.ca>
2025-01-15 22:21:37 -0500

    Added lab1b

---
+++ lab1b.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''Description: This script prints a greeting with a name.'''
+
+# Defining the string object "name"
+name = 'Isaac'
+
+# Printing the variable 'name'
+print('How old are you ' + name + '?')
